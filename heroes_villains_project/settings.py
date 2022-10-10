397ac3d0fa7347876079ad1b75417b92d0eab39e
Armando Beltran <armando_b@live.com>
2022-10-10 12:55:05 -0500

    Error testing

--- heroes_villains_project/settings.py
+++ heroes_villains_project/settings.py
@@ -35,6 +35,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    
 ]
 
 MIDDLEWARE = [
@@ -112,3 +114,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+try:
+    from heroes_villains_project import *
+except ImportError:
+    pass
