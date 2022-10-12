8d697f0a482ba8b0b69d5c9b6ab7d851829c2e82
Armando Beltran <armando_b@live.com>
2022-10-11 22:13:47 -0500

    Updated Supers Serializers file to allow proper data expression

--- supers/serializers.py
+++ supers/serializers.py
@@ -4,9 +4,6 @@ from .models import Super
 class SuperSerializer(serializers.ModelSerializer):
     class Meta:
         model = Super
-        fields = ['id', 'name', 'alter_ego', 'primary_ability', 'secondary_ability', 'catchphrase', 'super_type']
+        fields = ['id', 'name', 'alter_ego', 'primary_ability', 'secondary_ability', 'catchphrase', 'super_type', 'super_type_id']
         depth = 1
-        
-    # super_type = serializers.IntegerField(write_only=True) 
-    #^^^^^^^ when this is active my GET in postman shows all data in the entrys EXCEPT the super_type FK data
-    # when it's not I can't create a new entry because the "super_type_id" field is missing. 
+    super_type_id = serializers.IntegerField(write_only=True) 
